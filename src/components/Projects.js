c2794d863445b0af8869598b0ae7d2ddc0214407
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-10-27 13:40:56 +0300

    added deployement links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,13 +18,13 @@ const Projects = () => {
             description: `Asset inventory is designed to help organizations efficiently manage their assets. It includes features for tracking and organizing various assets, allowing users to add, update, and delete asset information. The system supports user authentication, ensuring secure access with role-based permissions for different user types, such as admins and regular employees. The project utilizes a backend framework like Flask for robust data management and a frontend framework like React for a user-friendly interface.`,
             image: project2,
             repoLink: 'https://github.com/calebkomen/Frontend-asset-inventory-management-phase5',
-            liveLink: 'https://project-two.com'
+            liveLink: 'https://assetfrontend.vercel.app/'
         },
         {
             title: 'TaskManager App',
             description: 'The Taskmaster app is a collaborative task management tool that allows users to create and assign tasks to their friends. Upon assignment, friends receive notifications about their new tasks. Users can easily organize their tasks using a drag-and-drop interface to move tasks between "To Do," "In Progress," and "Completed" categories. The app requires user authentication for secure access and is built using React for the frontend and Flask for the backend, ensuring a seamless and interactive user experience.',
             image: project3,
-            repoLink: 'https://github.com/yourusername/project-three',
+            repoLink: 'https://github.com/LEAKONO/Task-master',
             liveLink: 'https://project-three.com'
         },
     ];

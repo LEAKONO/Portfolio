9ee8dc28c12072292869a55a0549bc5006d48ef9
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-10-31 13:56:45 +0300

    changed skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaReact, FaPython, FaJsSquare, FaDatabase, FaHtml5, FaGithub } from 'react-icons/fa';
-import { SiTailwindcss, SiPostgresql, SiFlask, SiLinux } from 'react-icons/si';
+import { SiTailwindcss, SiPostgresql, SiFlask, SiLinux, SiSqlite, SiCss3, SiPostman } from 'react-icons/si';
 import './Skills.css'; // Import the CSS file for Skills
 
 const Skills = () => {
@@ -15,13 +15,15 @@ const Skills = () => {
                 <li><FaDatabase className="icon" /> SQL</li>
                 <li><SiPostgresql className="icon" /> PostgreSQL</li>
                 <li><FaHtml5 className="icon" /> HTML5</li>
+                <li><SiCss3 className="icon" /> CSS</li>
                 <li><SiFlask className="icon" /> Flask</li>
                 <li><FaGithub className="icon" /> GitHub</li>
                 <li><SiLinux className="icon" /> Linux</li>
+                <li><SiPostman className="icon" /> Postman</li>
+                <li><SiSqlite className="icon" /> SQLite3</li>
             </ul>
         </section>
     );
 };
 
 export default Skills;
-
